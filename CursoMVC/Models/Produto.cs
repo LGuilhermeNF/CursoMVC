f2ddc2850f331ecd5cfde1a83e85d24280b71a75
LGuilhermeNF <guilherme.doze@gmail.com>
2021-11-21 01:21:38 -0300

    Add project files.

---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
